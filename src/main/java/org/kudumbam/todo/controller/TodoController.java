837054198fa31546cbd1667fd6156bd76ff60c34
Mahalakshmi Yuvaraj <mahalakshmi.yuvaraj@gmail.com>
2019-08-07 20:52:02 -0700

    Todo microservice using Spring boot with unit tests

---
+++ src/main/java/org/kudumbam/todo/controller/TodoController.java
@@ -0,0 +1,49 @@
+package org.kudumbam.todo.controller;
+
+import java.net.URI;
+import java.util.List;
+
+import org.kudumbam.todo.model.Todo;
+import org.kudumbam.todo.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@RestController
+public class TodoController {
+	
+	@Autowired
+    private TodoService todoService;
+
+    @GetMapping("/users/{name}/todos")
+    public List<Todo> retrieveTodos(@PathVariable String name) {
+      return todoService.retrieveTodos(name);
+    }
+    
+    @GetMapping(path = "/users/{name}/todos/{id}")
+    public Todo retrieveTodo(@PathVariable String name, @PathVariable int id) {
+      return todoService.retrieveTodo(id);
+    }
+
+    @PostMapping("/users/{name}/todos")
+    ResponseEntity<?> add(@PathVariable String name,
+    @RequestBody Todo todo) { 
+      Todo createdTodo = todoService.addTodo(name, todo.getDesc(),
+      todo.getTargetDate(), todo.isDone());
+      if (createdTodo == null) {
+         return ResponseEntity.noContent().build();
+      }
+
+     URI location = ServletUriComponentsBuilder
+    		 .fromCurrentRequest()
+    		 .path("/{id}")
+    		 .buildAndExpand(createdTodo.getId())
+    		 .toUri();
+    return ResponseEntity.created(location).build();
+   }
+}
---
+++ src/test/java/org/kudumbam/todo/TodoControllerTest.java
@@ -0,0 +1,103 @@
+package org.kudumbam.todo;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.kudumbam.todo.controller.TodoController;
+import org.kudumbam.todo.model.Todo;
+import org.kudumbam.todo.service.TodoService;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(TodoController.class)
+public class TodoControllerTest {
+
+ @Autowired
+ private MockMvc mvc;
+
+ @MockBean
+ private TodoService service;
+
+ @Test
+ public void retrieveTodos() throws Exception {
+  List<Todo> mockList = Arrays.asList(new Todo(1, "Jack",
+  "Learn Spring MVC", new Date(), false), new Todo(2, "Jack",
+  "Learn Struts", new Date(), false));
+
+  when(service.retrieveTodos("Jack")).thenReturn(mockList);
+
+  MvcResult result = mvc
+		  .perform(MockMvcRequestBuilders
+		  .get("/users/Jack/todos")
+		  .accept(MediaType.APPLICATION_JSON))
+		  .andExpect(status().isOk())
+		  .andReturn();
+
+ String expected = "["
+  + "{id:1,user:Jack,desc:\"Learn Spring MVC\",done:false}" +","
+  + "{id:2,user:Jack,desc:\"Learn Struts\",done:false}" + "]";
+
+  JSONAssert.assertEquals(expected, result.getResponse()
+   .getContentAsString(), false);
+  }
+
+
+@Test
+  public void retrieveTodo() throws Exception {
+    Todo mockTodo = new Todo(1, "Jack", "Learn Spring MVC", 
+    new Date(), false);
+
+    when(service.retrieveTodo(1)).thenReturn(mockTodo);
+
+    MvcResult result = mvc.perform(
+    MockMvcRequestBuilders.get("/users/Jack/todos/1")
+    .accept(MediaType.APPLICATION_JSON))
+    .andExpect(status().isOk()).andReturn();
+
+    String expected = "{id:1,user:Jack,desc:\"Learn Spring MVC\",done:false}";
+
+   JSONAssert.assertEquals(expected, 
+    result.getResponse().getContentAsString(), false);
+
+  }
+
+
+@Test
+ public void createTodo() throws Exception {
+  Todo mockTodo = new Todo(1, "Jack", 
+  "Learn Spring MVC", new Date(), false);
+  String todo = "{\"user\":\"Jack\",\"desc\":\"Learn Spring MVC\",\"done\":false}";
+
+ when(service.addTodo("Jack", "Learn Spring MVC", null,false))
+ .thenReturn(mockTodo);
+
+mvc
+ .perform(MockMvcRequestBuilders.post("/users/Jack/todos")
+ .content(todo)
+ .contentType(MediaType.APPLICATION_JSON)
+ )
+ .andExpect(status().isCreated())
+ .andExpect(
+   header().string("location",containsString("/users/Jack/todos/"
+  + 1)));
+}
+
+}
